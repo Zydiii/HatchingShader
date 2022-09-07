3949c21b25a00ea833ead192dc1fbb7f898f42ee
zydiii <zydiiii@outlook.com>
2022-09-07 11:52:41 +0800

    init

---
+++ Source/HatchingShader/HatchingShader.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HatchingShader.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HatchingShader, "HatchingShader" );
